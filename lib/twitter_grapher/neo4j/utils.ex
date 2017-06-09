220e3bb00f56fd8758a6eb93d914cec0a8db4076
Omar Bahareth <omar@omar.engineer>
2017-06-09 16:18:14 +0300

    Add func to check if a node exists

--- lib/twitter_grapher/neo4j/utils.ex
+++ lib/twitter_grapher/neo4j/utils.ex
@@ -1,2 +1,8 @@
 defmodule Neo4j.Utils do
+  def exists(key: key, value: value) do
+    conn = Bolt.Sips.conn
+    query = "MATCH (n { #{key}: '#{value}' }) RETURN n"
+    result = Bolt.Sips.query!(conn, query)
+    length(result) != 0
+  end
 end
